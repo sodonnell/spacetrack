52657d65fed4afc68a1722e8011efe833521143a
Sean O'Donnell <sean@seanodonnell.com>
2018-11-16 11:05:47 -0800

    composerizing this old project. Initial branch commit.

---
+++ examples/spacetrack_boxscore.php.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+/** simple spacetrack API client script example using PHP/CLI scripting */
+require 'spacetrack.php';
+
+$spacetrack = spacetrack::getInstance();
+
+$api='boxscore';
+$postdata=null;
+$decode=false;
+
+$req_data = $spacetrack->api_call($api,$postdata,$decode);
+
+print $req_data;
+
